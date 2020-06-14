90992300f0b254de8d1f8e0723a12fdd9ab3de4b
Rafal Sokolowski <Rafal.Miroslaw.Sokolowski@gmail.com>
2020-06-14 13:14:52 +0200

    feat (engine): working on the map
    Preparing map logistics
    
    Open #001

--- src/main/java/pl/thgteam/thehexgame/TheHexGameApp.java
+++ src/main/java/pl/thgteam/thehexgame/TheHexGameApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThehexgameApplication {
+public class TheHexGameApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(ThehexgameApplication.class, args);
+        SpringApplication.run(TheHexGameApp.class, args);
     }
 
 }
